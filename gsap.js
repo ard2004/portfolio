a57827eeb619bc8c85cc79ca608adba46f91172a
captian-Arvoy <accofstardust@gmail.com>
2024-06-23 08:52:27 +0530

    feat: added style.css gsap.js

--- gsap.js
+++ gsap.js
@@ -12,26 +12,31 @@ const revealToSpan = () => {
     });
 };
 revealToSpan();
-var tl = gsap.timeline()
-tl.to(".child span",{
-    x: "-2%",
-    stagger: 0.1,
-    duration: 0.5,
-    delay: 0.2,
+var tl = gsap.timeline();
+tl.to(".child span", {
+  x: "-2%",
+  stagger: 0.1,
+  duration: 0.5,
+  delay: 0.2,
+});
+tl.to(".parent .child", {
+  y: "-100%",
+  duration: 0.80,
+  delay: 1,
+  ease: Circ.easeInOut,
+});
+tl.to("#loader", {
+  height: 0,
+  duration: 1,
+  ease: Expo.easeInOut,
 })
-tl.to(".parent .child",{
-    y: "-100%",
-    duration: 0.80,
-    delay: 1,
-    ease: Circ.easeInOut
-})
-tl.to("#loader",{
+  tl.to("#green", {
     height: 0,
+    delay: -.3,
     duration: 1,
-    ease: Expo.easeInOut
+  });
+  tl.to("#row #h1 h1",{
+    opacity: 100,
+    duration: 5,
+    stagger: 0.2
 })
-    .to("#green",{
-        height: 0,
-        delay: -.3,
-        duration: 1
-    })
